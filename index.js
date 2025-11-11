b7d3e93b8bd16ed18113146d95bf75ba72dd3ecf
morshed2006 <hussainmorshed141@gmail.com>
2025-11-11 07:31:39 -0500

    update

--- index.js
+++ index.js
@@ -1,37 +1,17 @@
+const express = require('express')
+const cors = require('cors')
+const admin = require("firebase-admin");
+const serviceAccount = require("./serviceKey.json");
+const app = express()
+require("dotenv").config()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { verifyToken } = require('./middleware/authMiddleware');
+const port = 5000
+app.use(cors())
+app.use(express.json())
 
 
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
-
-
-const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.9ay2t5q.mongodb.net/?appName=Cluster0`;
-
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-
-async function run() {
-  try {
-    await client.connect();
-    
-    const db = client.db('cleaner-db')
-    const cleanerCollection = db.collection('cleaner')
-
-    // Get all services
-    app.get('/cleaner', async (req, res) => {
-      try {
-        const result = await cleanerCollection.find().toArray()
-        res.send(result)
-      } catch (error) {
-        res.status(500).send({ error: 'Failed to fetch services' })
-      }
-    })
 
     // Get single service by ID
     app.get('/cleaner/:id',  async (req, res) => {
